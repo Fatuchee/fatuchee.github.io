4f275a06d39e1944dfe89f0569a0953e28e0e60b
Fatuchee <49082779+Fatuchee@users.noreply.github.com>
2023-02-26 18:18:19 +0100

    Added own art and fixed stuff

--- slide.js
+++ slide.js
@@ -1,79 +1,106 @@
 async function onLoad () {
-    let div = document.getElementById("comms");
-    let dots = document.createElement("div");
-    dots.setAttribute("id", "comm-dots");
-    dots.setAttribute("class", "dot-container")
+    let com = document.getElementById("com");
+    let own = document.getElementById("own");
+    let commsPics = [];
+    let ownPics = [];
+
     await fetch("pictures.json")
         .then(res => res.json())
-        .then(json => loadPics(json.picture, div, dots));
+        .then(json => loadPics(json.picture, commsPics, ownPics));
 
-    let temp = document.createElement("a");
-    temp.setAttribute("class", "prev");
-    temp.setAttribute("onclick", "plusSlides(-1)");
-    temp.innerHTML = "&#10094";
-    div.appendChild(temp);
-    temp = document.createElement("a");
-    temp.setAttribute("class", "next");
-    temp.setAttribute("onclick", "plusSlides(1)");
-    temp.innerHTML = "&#10095";
-    div.appendChild(temp);
-    div.appendChild(dots);
-    showSlides(1);
+
+    await createDiv(com, commsPics);
+    await createDiv(own, ownPics);
+
+    showSlides(1, "com");
+    showSlides(1, "own");
 }
 
 
-let slideIndex = 1;
+let slideIndexCom = 1;
+let slideIndexOwn = 1;
 onLoad();
-async function loadPics(json, doc, dots) {
-    console.log(json);
+async function loadPics(json, com, own) { 
+
     for (let i = 0; i < json.length; i++) {
         if (json[i].type === "com") {
-            createPic(doc, i+1, json.length, "comms", json[i].name, json[i].fileType, json[i].artist);
+            com.push(json[i]);
         } else if (json[i].type === "own") {
-            createPic(doc, i+1, json.length, "own", json[i].name, json[i].fileType, json[i].artist);
-        } else {
-            return;
+            own.push(json[i]);
         }
-        let dot = document.createElement("span");
-        dot.setAttribute("class", "dot");
-        dot.setAttribute("onclick", "currentSlide(" + (i+1) + ")");
-        dots.appendChild(dot);
     }
 }
 
-async function createPic(doc, num, max, type, name, fileType, author) {
+async function createDiv(n, nPics) {
+
+    let dots = document.createElement("div");
+    dots.setAttribute("id", nPics[0].type + "-dots");
+    dots.setAttribute("class", "dot-container")
+
+    for (let i = 0; i < nPics.length; i++) {
+        createPics(nPics[i].type, i+1, nPics.length, nPics[i].name, nPics[i].fileType, nPics[i].artist, n);
+        let dot = document.createElement("span");
+        dot.setAttribute("class", "dot-" + nPics[i].type);
+        dot.setAttribute("onclick", "currentSlide(" + (i+1) + ",'" + nPics[i].type + "')");
+        dots.appendChild(dot);
+    }
+
+    n.appendChild(dots);
+
+    let temp = document.createElement("a");
+    temp.setAttribute("class", "prev");
+    temp.setAttribute("onclick", "plusSlides(-1,'" + nPics[0].type + "')");
+    console.log(nPics[0].type);
+    temp.innerHTML = "&#10094";
+    n.appendChild(temp);
+    temp = document.createElement("a");
+    temp.setAttribute("class", "next");
+    temp.setAttribute("onclick", "plusSlides(1,'" + nPics[0].type + "')");
+    temp.innerHTML = "&#10095";
+    n.appendChild(temp);
+    n.appendChild(dots);
+}
+
+async function createPics(type, num, max, name, fileType, artist, doc) {
     let div = document.createElement("div");
-    div.setAttribute("class", "mySlides fade")
+    div.setAttribute("class", "mySlides-" + type + " fade");
     let tempDoc = document.createElement("div");
     tempDoc.innerHTML = num + " / " + max;
-    tempDoc.setAttribute("class", "numbertext")
+    tempDoc.setAttribute("class", "numbertext");
     div.appendChild(tempDoc);
     tempDoc = document.createElement("img");
     tempDoc.setAttribute("class", "img");
-    tempDoc.setAttribute("src", "pictures/" + type + "/" + name + "." + fileType)
+    tempDoc.setAttribute("src", "pictures/" + type + "/" + name + "." + fileType);
     div.appendChild(tempDoc);
     tempDoc = document.createElement("div");
     tempDoc.setAttribute("class", "text");
-    tempDoc.innerHTML = "Drawn by " + author;
+    tempDoc.innerHTML = "Drawn by " + artist;
     div.appendChild(tempDoc);
     doc.appendChild(div);
 }
 
-function plusSlides(n) {
-    showSlides(slideIndex += n)
+function plusSlides(n, type) {
+    if (type === "com") showSlides(slideIndexCom += n, type);
+    if (type === "own") showSlides(slideIndexOwn += n, type);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n)
+function currentSlide(n, type) {
+    if (type === "com") showSlides(slideIndexCom = n, type);
+    if (type === "own") showSlides(slideIndexOwn = n, type);
 }
 
-function showSlides(n) {
+function showSlides(n, type) {
     var i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    let slides = document.getElementsByClassName("mySlides-" + type);
+    let dots = document.getElementsByClassName("dot-" + type);
 
-    if (n > slides.length) slideIndex = 1;
-    if (n < 1) slideIndex = slides.length;
+    if (n > slides.length && type === "com") {
+        slideIndexCom = 1;
+    } else if (n > slides.length && type === "own") {
+        slideIndexOwn = 1;
+    }
+    if (n < 1 && type === "com") {slideIndexCom = slides.length;
+    } else if (n < 1 && type === "own") {slideIndexOwn = slides.length;}
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -83,6 +110,11 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    slides[slideIndex-1].style.display = "flex";
-    dots[slideIndex-1].className += " active";
+    if (type === "com") {
+        slides[slideIndexCom-1].style.display = "flex";
+        dots[slideIndexCom-1].className += " active";
+    } else if (type === "own") {
+        slides[slideIndexOwn-1].style.display = "flex";
+        dots[slideIndexOwn-1].className += " active";
+    }
 }
